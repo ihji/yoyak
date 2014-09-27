9739b27eb07b4009a01bce7b6d80670ac47e9e12
Heejong Lee <heejong@gmail.com>
2014-09-27 19:36:50 +0900

    impl MapDom

--- src/main/scala/com/simplytyped/yoyak/framework/domain/MapDom.scala
+++ src/main/scala/com/simplytyped/yoyak/framework/domain/MapDom.scala
@@ -3,15 +3,35 @@ package com.simplytyped.yoyak.framework.domain
 class MapDom[K,V](implicit val valueOps: LatticeOps[V]) {
   private var rawMap = Map.empty[K,V]
 
-  def update(kv: (K,V)) : MapDom[K,V] = { rawMap += (kv._1 -> kv._2); this }
+  private def add(kv: (K,V)) { if(!valueOps.<=(kv._2,valueOps.bottom)) rawMap += (kv._1 -> kv._2) }
+  def update(kv: (K,V)) : MapDom[K,V] = { add(kv); this }
   def weakUpdate(kv: (K,V)) : MapDom[K,V] = {
-    rawMap += (kv._1 -> valueOps.\/(get(kv._1),kv._2)); this
+    add(kv._1 -> valueOps.\/(get(kv._1),kv._2)); this
   }
   def get(k: K) : V = rawMap.getOrElse(k,valueOps.bottom)
-  def foldLeft[T](init: T)(f: (T,(K,V)) => T) : T = rawMap.foldLeft(init)(f)
 
-  implicit val ops = new LatticeOps[MapDom[K,V]] {
-    override def <=(lhs: MapDom[K, V], rhs: MapDom[K, V]): Boolean = ???
+  def foldLeft[T](init: T)(f: (T,(K,V)) => T) : T = rawMap.foldLeft(init)(f)
+  def size : Int = rawMap.size
+  def iterator = rawMap.iterator
+}
+
+object MapDom {
+  def empty[K,V](implicit valueOps: LatticeOps[V]) = new MapDom[K,V]
+
+  def ops[K,V](implicit valueOps: LatticeOps[V]) = new LatticeOps[MapDom[K,V]] {
+    override def <=(lhs: MapDom[K, V], rhs: MapDom[K, V]): Boolean = {
+      if(lhs.size > rhs.size) false
+      else {
+        val lhsIter = lhs.iterator
+        var flag = true
+        while(flag && lhsIter.hasNext) {
+          val (k,v) = lhsIter.next()
+          val rValue = rhs.get(k)
+          if(!valueOps.<=(v,rValue)) flag = false
+        }
+        flag
+      }
+    }
 
     override def \/(lhs: MapDom[K, V], rhs: MapDom[K, V]): MapDom[K, V] = {
       rhs.foldLeft(lhs) {case (map,(k,v)) => map.weakUpdate(k,v)}
@@ -19,9 +39,4 @@ class MapDom[K,V](implicit val valueOps: LatticeOps[V]) {
 
     override val bottom: MapDom[K, V] = MapDom.empty[K,V]
   }
-}
-
-object MapDom {
-  def empty[K,V](implicit valueOps: LatticeOps[V]) = new MapDom[K,V]
-
 }
---
+++ src/test/scala/com/simplytyped/yoyak/framework/domain/MapDomTest.scala
@@ -0,0 +1,76 @@
+package com.simplytyped.yoyak.framework.domain
+
+import org.scalatest.{Matchers, FunSuite}
+
+class MapDomTest extends FunSuite with Matchers {
+  test("MapDom ordering (<=) test: unrelated") {
+    import MapDomTest.ops
+    val map1 = MapDom.empty[Int,Set[Int]]
+    map1.update(1->Set(1,2,3))
+    val map2 = MapDom.empty[Int,Set[Int]]
+    map2.update(2->Set(1,2,3))
+    val ordering = MapDom.ops[Int,Set[Int]]
+
+    ordering.<=(map1,map2) should be (false)
+  }
+  test("MapDom ordering (<=) test: lhs is bigger") {
+    import MapDomTest.ops
+    val map1 = MapDom.empty[Int,Set[Int]]
+    map1.update(1->Set(1,2,3,4))
+    val map2 = MapDom.empty[Int,Set[Int]]
+    map2.update(1->Set(1,2,3))
+    val ordering = MapDom.ops[Int,Set[Int]]
+
+    ordering.<=(map1,map2) should be (false)
+  }
+  test("MapDom ordering (<=) test: rhs is bigger") {
+    import MapDomTest.ops
+    val map1 = MapDom.empty[Int,Set[Int]]
+    map1.update(1->Set(1,2,3))
+    val map2 = MapDom.empty[Int,Set[Int]]
+    map2.update(1->Set(1,2,3,4))
+    val ordering = MapDom.ops[Int,Set[Int]]
+
+    ordering.<=(map1,map2) should be (true)
+  }
+  test("MapDom ordering (<=) test: lhs has more elements") {
+    import MapDomTest.ops
+    val map1 = MapDom.empty[Int,Set[Int]]
+    map1.update(1->Set(1,2)).update(2->Set(2,3,4))
+    val map2 = MapDom.empty[Int,Set[Int]]
+    map2.update(1->Set(1,2,3))
+    val ordering = MapDom.ops[Int,Set[Int]]
+
+    ordering.<=(map1,map2) should be (false)
+  }
+  test("MapDom ordering (<=) test: rhs has more elements") {
+    import MapDomTest.ops
+    val map1 = MapDom.empty[Int,Set[Int]]
+    map1.update(1->Set(1,2,3))
+    val map2 = MapDom.empty[Int,Set[Int]]
+    map2.update(1->Set(1,2,3)).update(2->Set(2,3,4))
+    val ordering = MapDom.ops[Int,Set[Int]]
+
+    ordering.<=(map1,map2) should be (true)
+  }
+  test("MapDom ordering (<=) test: rhs has more elements, but lhs has a bigger element") {
+    import MapDomTest.ops
+    val map1 = MapDom.empty[Int,Set[Int]]
+    map1.update(1->Set(1,2,3,4))
+    val map2 = MapDom.empty[Int,Set[Int]]
+    map2.update(1->Set(1,2,3)).update(2->Set(2,3,4))
+    val ordering = MapDom.ops[Int,Set[Int]]
+
+    ordering.<=(map1,map2) should be (false)
+  }
+}
+
+object MapDomTest {
+  implicit val ops : LatticeOps[Set[Int]] = new LatticeOps[Set[Int]] {
+    override def <=(lhs: Set[Int], rhs: Set[Int]): Boolean =  lhs.subsetOf(rhs)
+
+    override def \/(lhs: Set[Int], rhs: Set[Int]): Set[Int] = lhs++rhs
+
+    override val bottom: Set[Int] = Set.empty
+  }
+}
