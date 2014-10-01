3eddb24f090ef1fb20568b0cef3cd1d79fc701d3
Heejong Lee <heejong@gmail.com>
2014-10-01 13:53:00 +0900

    MemDom WIP

--- src/main/scala/com/simplytyped/yoyak/framework/domain/MemDom.scala
+++ src/main/scala/com/simplytyped/yoyak/framework/domain/MemDom.scala
@@ -1,23 +1,82 @@
 package com.simplytyped.yoyak.framework.domain
 
 import com.simplytyped.yoyak.framework.domain.MemDom._
+import com.simplytyped.yoyak.il.CommonIL.ClassName
+import com.simplytyped.yoyak.il.CommonIL.Value._
 
 class MemDom[V](implicit val valueOps : LatticeOps[V]) {
   private var rawMap = MapDom.empty[AbsAddr,AbsValue[V]](AbsValue.ops[V])
-  def update(kv: (AbsAddr,AbsValue[V])) : MemDom[V] = {
+  def update(kv: (Loc,AbsValue[V])) : MemDom[V] = {
+    val (loc,v) = kv
+    val newRawMap = loc match {
+      case Local(id) => rawMap.update(AbsAddr(id)->v)
+      case ArrayRef(base,index) => ???
+      case InstanceFieldRef(base,field) =>
+        val objRef = get(base)
+        objRef match {
+          case AbsRef(ids) =>
+            ids.foldLeft(rawMap) {
+              (m,i) =>
+                val obj = rawMap.get(AbsAddr(i))
+                val newObj = obj match {
+                  case obj: AbsObject[V] =>
+                    if(ids.size == 1) obj.updateField(field->v)
+                    else obj.weakUpdateField(field->v)
+                  case _ => obj // error case: should be reported
+                }
+                m.update(AbsAddr(i)->newObj)
+            }
+          case _ => rawMap // error case: should be reported
+        }
+      case StaticFieldRef(clazz,field) =>
+        val staticAddr = getStaticAddr(clazz)
+        val staticObj = rawMap.get(staticAddr)
+        val newStaticObj = staticObj match {
+          case obj: AbsObject[V] =>
+            obj.weakUpdateField(field->v)
+          case _ =>
+            val obj = new AbsObject[V]
+            obj.updateField(field->v)
+        }
+        rawMap.update(staticAddr->newStaticObj)
+    }
     val newMemDom = new MemDom[V]
-    newMemDom.rawMap = rawMap.update(kv)
+    newMemDom.rawMap = newRawMap
     newMemDom
   }
-  def weakUpdate(kv: (AbsAddr,AbsValue[V])) : MemDom[V] = {
-    val newMemDom = new MemDom[V]
-    newMemDom.rawMap = rawMap.weakUpdate(kv)
-    newMemDom
-  }
+  def get(k: Loc) : AbsValue[V] =
+    k match {
+      case Local(id) => rawMap.get(AbsAddr(id))
+      case ArrayRef(base,index) => ???
+      case InstanceFieldRef(base,field) =>
+        val objRef = get(base)
+        objRef match {
+          case AbsRef(ids) =>
+            ids.foldLeft(AbsBottom: AbsValue[V]) {
+              (v,id) =>
+                val obj = rawMap.get(AbsAddr(id))
+                val fieldValue = obj match {
+                  case obj: AbsObject[V] => obj.getField(field)
+                  case _ => AbsBottom
+                }
+                AbsValue.ops[V].\/(v,fieldValue)
+            }
+          case _ => AbsBottom
+        }
+      case StaticFieldRef(clazz,field) =>
+        val staticAddr = getStaticAddr(clazz)
+        val obj = rawMap.get(staticAddr)
+        obj match {
+          case obj: AbsObject[V] => obj.getField(field)
+          case _ => AbsBottom
+        }
+    }
 }
 
 object MemDom {
   def empty[V](implicit valueOps : LatticeOps[V]) = new MemDom[V]
+
+  def getStaticAddr(className: ClassName) : AbsAddr = AbsAddr(s"__static_obj_${ClassName.toString(className)}")
 
   def ops[V](implicit valueOps : LatticeOps[V]) = new LatticeOps[MemDom[V]] {
     implicit val absValueOps = AbsValue.ops[V]
@@ -50,6 +109,19 @@ object MemDom {
     def <=(that: AbsObject[D]) : Boolean = {
       MapDom.ops[String,AbsValue[D]].<=(rawFieldMap,that.rawFieldMap)
     }
+    def updateField(kv: (String,AbsValue[D])) = {
+      val newFieldMap = rawFieldMap.update(kv)
+      val newObject = new AbsObject[D]
+      newObject.rawFieldMap = newFieldMap
+      newObject
+    }
+    def weakUpdateField(kv: (String,AbsValue[D])) = {
+      val newFieldMap = rawFieldMap.weakUpdate(kv)
+      val newObject = new AbsObject[D]
+      newObject.rawFieldMap = newFieldMap
+      newObject
+    }
+    def getField(k: String) : AbsValue[D] = rawFieldMap.get(k)
   }
   case class AbsRef(id: Set[String]) extends AbsValue
   case class AbsBox[D](data: D) extends AbsValue[D]
