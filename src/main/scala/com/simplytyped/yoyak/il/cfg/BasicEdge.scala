67aa8e171607f818e8df0d9538ab432288f70223
Heejong Lee <heejong@gmail.com>
2014-10-27 22:47:18 +0900

    add EdgeType

--- src/main/scala/com/simplytyped/yoyak/il/cfg/BasicEdge.scala
+++ src/main/scala/com/simplytyped/yoyak/il/cfg/BasicEdge.scala
@@ -1,8 +1,14 @@
 package com.simplytyped.yoyak.il.cfg
 
 import com.simplytyped.yoyak.graph.EdgeLike
+import com.simplytyped.yoyak.il.cfg.BasicEdge.{IntraEdge, EdgeType}
 
-case class BasicEdge(from: BasicBlock, to: BasicBlock) extends EdgeLike[BasicBlock] {
-  type L = Option[Nothing]
-  val label = None
+case class BasicEdge(from: BasicBlock, to: BasicBlock, label: EdgeType = IntraEdge) extends EdgeLike[BasicBlock] {
+  type L = EdgeType
+}
+
+object BasicEdge {
+  abstract class EdgeType
+  case object InterEdge extends EdgeType
+  case object IntraEdge extends EdgeType
 }
