b1daf4de8befdaea4b050ab86722ac9acab64200
Heejong Lee <heejong@gmail.com>
2015-07-07 10:19:47 +0900

    minor brushup

--- src/main/scala/com/simplytyped/yoyak/analysis/IntervalAnalysis.scala
+++ src/main/scala/com/simplytyped/yoyak/analysis/IntervalAnalysis.scala
@@ -1,5 +1,6 @@
 package com.simplytyped.yoyak.analysis
 
+import com.simplytyped.yoyak.analysis.IntervalAnalysis.GMemory
 import com.simplytyped.yoyak.framework.ForwardAnalysis.FlowSensitiveForwardAnalysis
 import com.simplytyped.yoyak.framework.domain.Galois.{SetAbstraction, GaloisIdentity}
 import com.simplytyped.yoyak.framework.domain.{ArithmeticOps, LatticeOps}
@@ -11,19 +12,22 @@ import com.simplytyped.yoyak.il.cfg.CFG
 class IntervalAnalysis(cfg: CFG) {
   def run() = {
     import IntervalAnalysis.{memDomOps,absTransfer,widening}
-    val analysis = new FlowSensitiveForwardAnalysis[GaloisIdentity[MemDom[IntervalInt,SetAbstraction[Any]]]](cfg)
+    val analysis = new FlowSensitiveForwardAnalysis[GMemory](cfg)
     val output = analysis.compute
     output
   }
 }
 
 object IntervalAnalysis {
-  implicit val absTransfer : AbstractTransferable[GaloisIdentity[MemDom[IntervalInt,SetAbstraction[Any]]]] = new StdSemantics[IntervalInt,SetAbstraction[Any],MemDom[IntervalInt,SetAbstraction[Any]]] {
-    val arithOps: ArithmeticOps[IntervalInt] = IntervalInt.arithOps
-  }
+  type Memory = MemDom[IntervalInt,SetAbstraction[Any]]
+  type GMemory = GaloisIdentity[Memory]
+  implicit val absTransfer : AbstractTransferable[GMemory] =
+    new StdSemantics[IntervalInt,SetAbstraction[Any],Memory] {
+      val arithOps: ArithmeticOps[IntervalInt] = IntervalInt.arithOps
+    }
 
-  implicit val memDomOps : LatticeOps[GaloisIdentity[MemDom[IntervalInt,SetAbstraction[Any]]]] = MemDom.ops[IntervalInt,SetAbstraction[Any]]
-  implicit val widening : Option[Widening[GaloisIdentity[MemDom[IntervalInt,SetAbstraction[Any]]]]] = {
+  implicit val memDomOps : LatticeOps[GMemory] = MemDom.ops[IntervalInt,SetAbstraction[Any]]
+  implicit val widening : Option[Widening[GMemory]] = {
     implicit val NoWideningForSetAbstraction = Widening.NoWidening[SetAbstraction[Any]]
     Some(MemDom.widening[IntervalInt,SetAbstraction[Any]])
   }
